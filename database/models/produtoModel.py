e72cb730444ef61b6d0b814229ec69285d79ddaa
davi <davicarvalho.b1@gmail.com>
2025-07-20 20:01:31 -0300

    feat: __repr__

--- database/models/produtoModel.py
+++ database/models/produtoModel.py
@@ -3,3 +3,6 @@ class Produto:
         self.produto_id = produto_id
         self.nome = nome
         self.preco_unidade = preco_unidade
+
+    def __repr__(self):
+        return (f"Produto ID={self.produto_id}, Nome='{self.nome}', Preço Unidade={self.preco_unidade}")
