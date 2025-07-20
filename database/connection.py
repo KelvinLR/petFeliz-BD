36b0eddbd0b09036560cdaa22b1466125b9b0046
davi <davicarvalho.b1@gmail.com>
2025-07-20 17:18:17 -0300

    feat: use env to set configurations

--- database/connection.py
+++ database/connection.py
@@ -1,11 +1,15 @@
 import psycopg2
-from config.db_config import DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_connection():
     return psycopg2.connect(
-        host=DB_HOST,
-        port=DB_PORT,
-        database=DB_NAME,
-        user=DB_USER,
-        password=DB_PASSWORD
+        host=os.getenv("DB_HOST", "localhost"),
+        port=int(os.getenv("DB_PORT", 5432)),
+        database=os.getenv("DB_NAME", "petshop"),
+        user=os.getenv("DB_USER", "postgres"),
+        password=os.getenv("DB_PASSWORD", None)
     )
