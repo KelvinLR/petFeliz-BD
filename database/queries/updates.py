0afb2d169de873bb7714997788d6c356b45b0158
davi <davicarvalho.b1@gmail.com>
2025-07-20 16:51:40 -0300

    refactor: update query

--- database/queries/updates.py
+++ database/queries/updates.py
@@ -1,27 +1,36 @@
-from database.connection import get_connection
+from database.queries.services import Services
+from models.funcionario import Funcionario
 from models.pet import Pet
 from models.cliente import Cliente
 
-def cliente_update(cliente):
-    conn = get_connection()
-    cur = conn.cursor()
-    cur.execute("""
+update = Services().execute_query
+
+def cliente_update(cliente: Cliente):
+    query = """
         UPDATE Cliente
         SET cpf = %s, nome = %s, telefone = %s, endereco = %s
         WHERE clienteid = %s;
-    """, (cliente.cpf, cliente.nome, cliente.telefone, cliente.endereco, cliente.clienteid))
-    conn.commit()
-    cur.close()
-    conn.close()
+    """
+    params = (cliente.cpf, cliente.nome, cliente.telefone, cliente.endereco, cliente.clienteid)
 
-def pet_update(pet):
-    conn = get_connection()
-    cur = conn.cursor()
-    cur.execute("""
+    update(query=query, params=params)
+
+def pet_update(pet: Pet):
+    query = """
         UPDATE Pet
-        SET petid = %s, nome = %s, idade = %s, datanascimento = %s, raca = %s, especie = %s, clientecpf = %s
+        SET nome = %s, idade = %s, datanascimento = %s, raca = %s, especie = %s, clientecpf = %s
         WHERE petid = %s
-    """, (pet.petid, pet.nome, pet.idade, pet.datanascimento, pet.raca, pet.especie, pet.clientecpf))
-    conn.commit()
-    cur.close()
-    conn.close()
+    """
+    params = (pet.nome, pet.idade, pet.datanascimento, pet.raca, pet.especie, pet.clientecpf, pet.petid)
+    
+    update(query=query, params=params)
+
+def funcionario_update(funcionario: Funcionario):
+    query = """
+        UPDATE Funcionario
+        SET nome = %s, especialidade = %s, nome = %s, salario = %s
+        WHERE funcionarioid = %s
+    """
+    params = (funcionario.nome, funcionario.especialidade, funcionario.salario, funcionario.funcionarioid, )
+    
+    update(query=query, params=params)
