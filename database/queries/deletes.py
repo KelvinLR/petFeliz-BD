7bde0994bb08c5cc25702174877cbe5425bed1ed
davi <davicarvalho.b1@gmail.com>
2025-07-20 16:50:13 -0300

    refactor: delete query

--- database/queries/deletes.py
+++ database/queries/deletes.py
@@ -1,14 +1,16 @@
-from database.connection import get_connection
+from database.queries.services import Services
 from models.pet import Pet
 from models.cliente import Cliente
 
+delete = Services().execute_query
+
 def cliente_delete(clienteid):
-    conn = get_connection()
-    cur = conn.cursor()
-    cur.execute("DELETE FROM Cliente WHERE clienteid = %s;", (clienteid,))
-    conn.commit()
-    cur.close()
-    conn.close()
+    query = "DELETE FROM Cliente WHERE clienteid = %s;"
+    delete(query=query, params=(clienteid,))
+
+def cliente_delete_by_cpf(clientecpf):
+    query = "DELETE FROM Cliente WHERE cpf = %s;"
+    delete(query=query, params=(clientecpf,))
 
 def cliente_delete_by_cpf(clientecpf):
     conn = get_connection()
@@ -19,9 +21,9 @@ def cliente_delete_by_cpf(clientecpf):
     conn.close()
 
 def pet_delete(petid):
-    conn = get_connection()
-    cur = conn.cursor()
-    cur.execute("DELETE FROM Cliente WHERE petid = %s;", (petid,))
-    conn.commit()
-    cur.close()
-    conn.close()
+    query = "DELETE FROM Pet WHERE petid = %s;"
+    delete(query=query, params=(petid,))
+
+def funcionario_delete(funcionarioid):
+    query = "DELETE FROM Funcionario WHERE funcionarioid = %s;"
+    delete(query=query, params=(funcionarioid,))
